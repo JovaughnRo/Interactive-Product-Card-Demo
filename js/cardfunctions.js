9ba631e2cb8b6cd64adf414ae27daf25ff330085
Jovaughn Rowe <49598724+JovaughnRo@users.noreply.github.com>
2026-06-02 20:10:59 -0400

    Update for v6 of the product card

--- js/cardfunctions.js
+++ js/cardfunctions.js
@@ -12,31 +12,59 @@ jQuery(document).ready(function ($) {
 	} else {
 		$dID = "category";
 	}
-	// localHost access -> "json/modelsDatabase.json"
-    // remote -> "https://jovaughnro.github.io/Interactive-Product-Card-Demo/json/modelsDatabase.json"
-	$.getJSON("https://jovaughnro.github.io/Interactive-Product-Card-Demo/json/modelsDatabase.json", function (data) {
+    // local host -> "/js/modelsDatabase.json"
+	// remote -> "https://marathonequipmentinc.com/wp-content/themes/oceanwp-child/js/modelsDatabase.json"
+	$.getJSON("json/modelsDatabase.json", function (data) {
 		$masterOModels = data;
 	});
     
 	maincontroller();
 	function maincontroller() {
 		// when the tank capacity buttons are clicked
-		$(".bgtc-class")
+		$(".bgtc-class, .mobile-model-options-ddm")
 			.children()
 			.on("click", function () {
 				// $modelSeriesId = mobileidDatabase($selectModel); // gets the model family ie: MM
 				$id = $(this).attr("id");
 				$model = $id.split("Btn")[0];
-
+				// console.log($id);
 				updateMachineModels($model); // gets the selected product then print it out on screen.
 				activemobiletable($model);
 				$btnGroup = $("#" + $id).parent();
+				// console.log($btnGroup);
 
 				// this toggle the active button color for the tank capacity
-				$("#" + $btnGroup.attr("id"))
-					.children()
-					.removeClass("act");
-				$("#" + $id).addClass("act");
+				// $("#" + $btnGroup.attr("id"))
+				// 	.children()
+				// 	.removeClass("act");
+				// $("#" + $id).addClass("act");
+
+				// switch to sibilings
+				// this toggle the active button color
+				$(this).addClass("act");
+				$(this).siblings().removeClass("act");
+				// if mobile models option is clicked. then make desktop tank capacity button match active color
+				if ($(this).attr("id").includes("-Mobile")) {
+					$desktopBtn = $id.split("-Mobile")[0];
+
+					$ptordt = $id.split("Btn-Mobile")[0].slice(-2); // PT or DT if button id is KEB500DTBtn-Mobile it gets the substring "DT"
+					$mobile_ddm_model_id = $btnGroup.attr("id").split("ddm-mobile-")[1];
+					switch ($mobile_ddm_model_id) {
+						case "keb":
+						case "ld":
+						case "ucmk":
+							$("button#" + $mobile_ddm_model_id + "Btn" + $ptordt).trigger("click");
+							break;
+					}
+					$("button#" + $desktopBtn).trigger("click");
+				} else {
+					// if desktop tank capacity buttons are clicked the mobile model option will change the active color to match
+					// which tank capacity was selected.
+					$("#" + $id + "-Mobile").addClass("act");
+					$("#" + $id + "-Mobile")
+						.siblings()
+						.removeClass("act");
+				}
 			});
 
 		$viewID = $("main").attr("id");
@@ -158,23 +186,6 @@ jQuery(document).ready(function ($) {
 										.find("div#UCMKDT_Specscontainer")
 										.addClass("d-none");
 									break;
-								case "keb":
-									$("button#kebdtSpecs").attr("id", "kebSpecs");
-									$("button#KEB115PTBtn").removeClass("d-none");
-									$("div#KEBPT_Specscontainer")
-										.removeClass("d-none")
-										.end()
-										.find("div#KEBDT_Specscontainer")
-										.addClass("d-none");
-									break;
-								case "ld":
-									$("button#lddtSpecs").attr("id", "ldSpecs");
-									$("div#LDPT_Specscontainer")
-										.removeClass("d-none")
-										.end()
-										.find("div#LDDT_Specscontainer")
-										.addClass("d-none");
-									break;
 							}
 						});
 						break;
@@ -183,7 +194,22 @@ jQuery(document).ready(function ($) {
 			});
 		}
 	}
-	function modelSwitch($modelTitle, $commonTitle, $bl, $tc, $bt, $ft, $img, $desc, $learn, $inquiry, $alt, $category) {
+	function modelSwitch(
+		$modelTitle,
+		$commonTitle,
+		$bl,
+		$tc,
+		$bt,
+		$ft,
+		$img,
+		$desc,
+		$learn,
+		$inquiry,
+		$alt,
+		$category,
+		$viewBrochureLink,
+		$buttonTitle,
+	) {
 		this.$modelTitle = $modelTitle;
 		this.$commonTitle = $commonTitle;
 		this.$bl = $bl;
@@ -196,58 +222,51 @@ jQuery(document).ready(function ($) {
 		this.$learn = $learn;
 		this.$inquiry = $inquiry;
 		this.$category = $category;
+		this.$viewBrochureLink = $viewBrochureLink;
+		this.$buttonTitle = $buttonTitle;
+
+		//console.log($viewBrochureLink); // <- this works
 		this.update = function () {
 			document.getElementById($category + "-modelId").innerHTML = "Model: " + this.$modelTitle;
 			// hot fix for ucmk pt and dt brochure
-			switch (this.$modelTitle) {
-				case "UCMK130PT":
-				case "UCMK230PT":
-				case "UCMK370PT":
-					document.getElementById("ucmk-broc").href =
-						"https://marathonequipmentinc.com/wp-content/uploads/2025/09/marathon-equipment-inc-ucmkpt-brochure.pdf";
-					break;
-				case "UCMK130DT":
-				case "UCMK230DT":
-				case "UCMK370DT":
-					document.getElementById("ucmk-broc").href =
-						"https://marathonequipmentinc.com/wp-content/uploads/2023/10/marathon-equipment-inc-ucmkdt-brochure.pdf";
-					break;
-				
-			}
-			if (this.$modelTitle == "DPS9") {
-				document.getElementById($category + "-modelSpesc").innerHTML = this.$ft;
-				// we are going to add brochure change for the dps here make sure to call the id dps-broc
-				// switch dps9 brochure
-				document.getElementById('dps-broc').href = "https://marathonequipmentinc.com/wp-content/uploads/2022/04/marathon-equipment-inc-dps9-brochure.pdf";
-			} else if (this.$modelTitle == "DPS550T") {
-				document.getElementById($category + "-modelSpesc").innerHTML =
-					this.$tc + " | " + this.$bt + " | " + this.$ft;
-					// switch dps550t brochure 
-					document.getElementById('dps-broc').href = "https://marathonequipmentinc.com/wp-content/uploads/2025/06/marathon-equipment-inc-dps550t-brochure.pdf";
-			} else if (this.$modelTitle != "DPS550T" && this.$modelTitle != "DPS9") {
+
+			//console.log("GRACO CAT -> ", $category);
+
+			if (this.$modelTitle != "DPS550T" && this.$modelTitle != "DPS9") {
 				switch ($category) {
+					case "GLVS":
+					case "HALS":
+						break;
 					case "cr":
 					case "rcr":
 					case "hepr":
 						document.getElementById($category + "-modelSpesc").innerHTML =
 							this.$tc + " | " + this.$bt + " | " + this.$ft;
-						break;						
+						break;
+
 					default:
 						if (this.$tc == "N/A" && this.$bt != "N/A") {
-							document.getElementById($category + "-modelSpesc").innerHTML = this.$bt + "-Fired | " + this.$ft;
+							document.getElementById($category + "-modelSpesc").innerHTML = this.$bt + "-Fired | " + this.$ft; // desktop
+							document.getElementById($category + "-mobile-modelSpesc").innerHTML = this.$bt + "-Fired | " + this.$ft; //mobile
 						} else if (this.$tc == "N/A" && this.$bt == "N/A") {
-							document.getElementById($category + "-modelSpesc").innerHTML = this.$ft;
+							document.getElementById($category + "-modelSpesc").innerHTML = this.$ft; // desktop
+							document.getElementById($category + "-mobile-modelSpesc").innerHTML = this.$ft; //mobile
 						} else if (this.$tc != "N/A" && this.$bt == "N/A") {
-							document.getElementById($category + "-modelSpesc").innerHTML = this.$tc + " | " + this.$ft;
+							document.getElementById($category + "-modelSpesc").innerHTML = this.$tc + " | " + this.$ft; // desktop
+							document.getElementById($category + "-mobile-modelSpesc").innerHTML = this.$tc + " | " + this.$ft; // mobile
 						} else if (this.$tc != "N/A" && this.$bt != "N/A") {
 							document.getElementById($category + "-modelSpesc").innerHTML =
-								this.$tc + " | " + this.$bt + "-Fired | " + this.$ft;
+								this.$tc + " | " + this.$bt + "-Fired | " + this.$ft; // desktop
+							document.getElementById($category + "-mobile-modelSpesc").innerHTML =
+								this.$tc + " | " + this.$bt + "-Fired | " + this.$ft; //mobile
 						}
 						break;
 				}
 			}
+
 			if (this.$bl != "*") {
 				document.getElementById("desktop-" + $category + "-bl-link").href = this.$bl;
+				document.getElementById("mobile-" + $category + "-bl-link").href = this.$bl;
 			}
 			document.getElementById($category + "-ModelTitle").innerHTML = this.$commonTitle;
 			document.getElementById($category + "ProductDescrip").innerHTML = this.$desc;
@@ -255,6 +274,53 @@ jQuery(document).ready(function ($) {
 			document.getElementById($category + "-gaq").href = this.$inquiry;
 			document.getElementById($category + "-imgDisplay").src = this.$img;
 			document.getElementById($category + "-imgDisplay").alt = this.$alt;
+			switch ($category) {
+				case "GLVS":
+				case "HALS":
+					break;
+
+				default:
+					document.getElementById($category + "-brochure").href = this.$viewBrochureLink;
+					// For Mobile product cards
+					document.getElementById($category + "-mobile-modelId").innerHTML = this.$buttonTitle;
+					break;
+			}
+
+			switch ($category) {
+				case "GLVS":
+				case "HALS":
+				case "cr":
+				case "pas":
+					// if the model has no burner or engine type. Do nothing
+					break;
+				case "pes":
+					if (this.$bt == "N/A") {
+						document.getElementById($category + "-mobile-bt").innerHTML = "";
+						$("#pes-mobile-bt").addClass("d-none");
+					} else {
+						//console.log($category);
+						document.getElementById($category + "-mobile-bt").innerHTML = " | " + this.$bt + "-Fired";
+						$("#pes-mobile-bt").removeClass("d-none");
+					}
+					break;
+				default:
+					document.getElementById($category + "-mobile-bt").innerHTML = this.$bt + "-Fired";
+					break;
+			}
+
+			switch ($category) {
+				case "GLVS":
+				case "HALS":
+					break;
+				default:
+					document.getElementById($category + "-mobile-imgDisplay").src = this.$img;
+					document.getElementById($category + "-mobile-imgDisplay").alt = this.$alt;
+					document.getElementById($category + "-mobile-ModelTitle").innerHTML = this.$commonTitle;
+					document.getElementById($category + "-mobile-LM").href = this.$learn;
+					document.getElementById($category + "-mobile-img-learnmore-link").href = this.$learn;
+					document.getElementById($category + "-mobile-gaq").href = this.$inquiry;
+					break;
+			}
 		};
 	}
 
@@ -330,9 +396,9 @@ jQuery(document).ready(function ($) {
 	function updateMachineModels($selectModel, $dID, $btn_cat) {
 		// please note: always include (available:) key to all objects to ensure that the database works.
 		// please note: organize data in order  largest of gallon size to the smallest. the 'size' key will represent this value.
-        
+
 		if ($dID != "home" && $dID != "mobile") {
-           
+			// this effects the product cards;
 			for ($i = 0; $i < $masterOModels.length; $i++) {
 				if ($selectModel == $masterOModels[$i].name) {
 					$oModels = new modelSwitch(
@@ -347,13 +413,66 @@ jQuery(document).ready(function ($) {
 						$masterOModels[$i].learnLink,
 						$masterOModels[$i].inquiryLink,
 						$masterOModels[$i].alt,
-						$masterOModels[$i].category
+						$masterOModels[$i].category,
+						$masterOModels[$i].viewBrochureLink,
+						$masterOModels[$i].buttonTitle,
 					);
 					$oModels.update();
 				}
 			}
-			
+			// filters and sorts models based on a selected category - used for the homepage filter system
+			// if a category is selected, the results are filtered based on the category and then sorted alphabetically
+			if ($selectModel == null && $btn_cat != null) {
+				$result = [];
+				for ($i = 0; $i < $masterOModels.length; $i++) {
+					if ($masterOModels[$i].available.includes($btn_cat) == true) {
+						$result.push($masterOModels[$i]);
+					}
+				}
+				// this sorts the results list in alphabetical order based on the name value.
+				$result.sort(function ($a, $b) {
+					return $a.name.localeCompare($b.name);
+				});
+				prevCardBuild($result, $btn_cat);
+			}
 		}
+		// updating home page modal
+		if ($selectModel != null && $dID == "home") {
+			// this loads model info that has both diesel and propane options
+			if ($btn_cat != "modelSwitch") {
+				for ($i = 0; $i < $masterOModels.length; $i++) {
+					if ($selectModel == $masterOModels[$i].name) {
+						$txt = $masterOModels[$i].descp[$btn_cat];
+						$oModels = new modalCard(
+							$masterOModels[$i].commonTitle,
+							$masterOModels[$i].tankCapacity,
+							$masterOModels[$i].burnerType,
+							$masterOModels[$i].frameType,
+							$masterOModels[$i].image,
+							$txt,
+							$masterOModels[$i].alt,
+							$masterOModels[$i].learnLink,
+							$masterOModels[$i].inquiryLink,
+						);
+						$oModels.update();
+					}
+				}
+			} else {
+				for ($i = 0; $i < $masterOModels.length; $i++) {
+					if ($selectModel == $masterOModels[$i].name) {
+						$oModels = new modalSwitch(
+							$masterOModels[$i].burnerType,
+							$masterOModels[$i].image,
+							$masterOModels[$i].alt,
+							$masterOModels[$i].learnLink,
+							$masterOModels[$i].inquiryLink,
+						);
+						$oModels.update();
+					}
+				}
+			}
+		}
+
 		if ($selectModel != null && $dID == "mobile") {
 			for ($i = 0; $i < $masterOModels.length; $i++) {
 				if ($selectModel == $masterOModels[$i].name) {
@@ -366,7 +485,7 @@ jQuery(document).ready(function ($) {
 						$masterOModels[$i].descp["default"],
 						$masterOModels[$i].alt,
 						$masterOModels[$i].learnLink,
-						$masterOModels[$i].inquiryLink
+						$masterOModels[$i].inquiryLink,
 					);
 					$mobileModels.update($modelseriesID);
 				}
@@ -406,6 +525,3 @@ jQuery(document).ready(function ($) {
 		// for DF10W and hides the others until they are selected.
 	}	
 });
-
-
-
